fae06b32ed2aa7da03306204ddf86777abddfa0e
fabuloso01 <fabuloso01@github.com>
2022-07-21 15:09:32 -0400

    commit

--- scripts.js
+++ scripts.js
@@ -12,10 +12,30 @@ if (hora > 6 && hora < 12) {
 else if (hora > 12 && hora < 18) {
     console.log('Boa tarde!')   
 
-    
+
 }
 else {
     console.log("Boa noite!")
 }
 */
+
+let permissao;
+permissao = ""
+
+switch (permissao) {
+    case "comum":
+    console.log("usuário comum!");
+    break;
+
+    case "gerente":
+    console.log("usuário gerente!");
+    break;
+
+    default:
+    console.log("Usuário não reconecido!");
+
+}     
+        
+
+    
 
